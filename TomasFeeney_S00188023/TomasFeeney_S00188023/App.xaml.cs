006ece880ac95b1cf4bd93fe77ea9b0ad1b690c6
TomasFeeneySligo <TomasFeeneySligo@users.noreply.github.com>
2020-09-02 10:10:05 +0000

    ProjectSetup

---
+++ TomasFeeney_S00188023/TomasFeeney_S00188023/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TomasFeeney_S00188023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
